6be423a1e1fe22b9d76f85e8effd6794fd197c8f
Mina-ah <minaparveenn@gmail.com>
2024-02-12 18:50:41 +0530

    basic pattern

--- pattern1.java
+++ pattern1.java
@@ -1,9 +1,17 @@
 /**
  * pattern1
+ * we need to know three major point while solving this
+ * 1- how many rows are there
+ * 2- for each row how many columns are there
+ * 3- and what we need to print
  */
 public class pattern1 {
 public static void main(String args[]){
-System.out.println("hello world");
-System.out.println("bye world");
+for(int i=1;i<=4;i++){
+    for(int j=1;j<=4-i+1;j++){
+        System.out.print("*");
+    }
+    System.out.println("");
+}
 }
 }
