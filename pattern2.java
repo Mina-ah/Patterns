f4c2c279e6d891c19b7688eee90e42e861ed6bd0
Mina-ah <minaparveenn@gmail.com>
2024-02-29 16:28:14 +0530

    rechecking the commits

---
+++ pattern2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class pattern2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter row number");
+        int n=sc.nextInt();
+        System.out.println(n);
+        for("")
+    }
+}
